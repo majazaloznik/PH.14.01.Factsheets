2ebc2c4e21c7d6a575d15a0222a28f6e184405c2
majazaloznik <maja.zaloznik@gmail.com>
2018-01-10 15:26:57 +0000

    stuff from last year, some plotting i think

--- code/data-plotting/03-data-plotting.R
+++ code/data-plotting/03-data-plotting.R
@@ -5,87 +5,51 @@
 ## 1. Plotting ################################################################
 ###############################################################################
 ## 0. Preliminaries  ##########################################################
-library(eulerr)
-library(plotrix) # for circles
 options(stringsAsFactors = FALSE)
 catalog <- read.csv("data/processed/catalog.final.csv")
 source("code/functions/FunPlotSimple.R")
 .oldpar <- par()
+
 ## 0.1 clean slate ############################################################
 
 
-# # plot in base
-FunPlotBase <- function(i) {
-  col1 <- rgb(121, 177, 216, 100, maxColorValue = 255)
-  col1.b <- rgb(121, 177, 216, 255, maxColorValue = 255)
-  col2 <- rgb(249, 209, 89, 100, maxColorValue = 255)
-  col2.b <- rgb(249, 209, 89, 255, maxColorValue = 255)
-  df <- readRDS(catalog$final.rds[i])
-  pt <- prop.table(xtabs(df$weight ~ df$var + df$region), 2)
-  fit <-
-    euler(
-      c(
-        "wife-urban" = pt[1, 1],
-        "husband-urban" = pt[2, 1],
-        "wife-urban&husband-urban" = pt[3, 1],
-        "wife-rural" = pt[1, 2],
-        "husband-rural" = pt[2, 2],
-        "wife-rural&husband-rural" = pt[3, 2]
-      )
-    )
-  
-  f <- fit$coefficients
-  par(mar = c(1, 1, 1, 1))
-  dist.u <- ((f[1, 1] - f[2, 1]) ^ 2 + (f[1, 2] - f[2, 2]) ^ 2) ^ 0.5
-  dist.r <- ((f[3, 1] - f[4, 1]) ^ 2 + (f[3, 2] - f[4, 2]) ^ 2) ^ 0.5
-  plot(
-    1,
-    xlim = c(-1.5, 1.5),
-    ylim = c(-1, 1),
-    type = "n",
-    bty = "n",
-    ylab = "",
-    xlab = "",
-    axes = FALSE
-  )
-  mtext(paste(catalog$country[i], catalog$year[i], sep = " - "))
-  draw.circle(-0.7, 0, f[3, 3], col = col1,
-              border = col1.b)
-  draw.circle(
-    -0.7,
-    dist.r,
-    f[4, 3],
-    col = col2,
-    border = col2.b
-  )
-  draw.circle(0.7, 0, f[1, 3], 
-              col = col1, 
-              border = col1.b)
-  draw.circle(0.7,
-              dist.u,
-              f[2, 3],
-              col = col2,
-              border = col2.b)
-}
+
+pal <- qualpal(n = 3, list(h = c(0, 360), s = c(0.4, 0.6), l = c(0.6, 0.8)),
+               cvd = "deutan", cvd_severity = 0.5)
+
+FunPlotBar(44, pal)
 
 
-## 1. Plotting ################################################################
+# colours for euler
+pal <- qualpal(n = 2, list(h = c(0, 180), s = c(0.4, 0.6), l = c(0.6, 0.85)))
+plot(pal)
+w1 <- pal$RGB[1,1]
+w2 <- pal$RGB[1,2]
+w3 <- pal$RGB[1,3]
+h1 <- pal$RGB[2,1]
+h2 <- pal$RGB[2,2]
+h3 <- pal$RGB[2,3]
 
+alpha = 0.4
+col1 <- rgb(w1, w2, w3, alpha)
+col1.b <- rgb(1,1,1,0.7)
+col2 <- rgb(h1, h2, h3, alpha)
+col2.b <- rgb(1,1,1,0.7)
+
+# plot euler diagrams
 cairo_ps("figures/test2.eps", width = 12, height = 12)
 layout(matrix(c(1:12), nrow = 4))
+par(mar = c(1, 1, 1, 1))
 for(i in 1:12){
-FunPlotBase(i)
+  FunPlotBase(i, col1, col1.b, col2, col2.b)
 }
 dev.off()
+par() <-.oldpar
+
 
 cairo_ps("figures/test1.eps")
 FunPlotSimple(i)
 dev.off()
 
 
-
-i = 123
-
-#
-# prop.table(table(AFIR70$region, AFIR70$var)[,1:3], 1)
-# barplot(t(as.matrix(prop.table(table(AZIR52$region, AZIR52$var)[,1:3], 1))))
+par() <-.oldpar
--- code/functions/FunPlotSimple.R
+++ code/functions/FunPlotSimple.R
@@ -1,9 +1,79 @@
+#' FunPlotBase
+#' 
+#' A base graphics verion of the `eulerr` plot - because I only 
+#' have two circles intersecting I want the axis to remain constant
+#' but eulerr has a stochastic element in the algorithm.
+#' 
+#' @i the row in the catalog for the survey
+#' 
+#' @col1 the colour for the wives' circle in rgb format with alpha!
+#' @col1.b the colour of the wives' circle border
+#' 
+#' @col2 the colour for the husbands' circle in rgb format with alpha!
+#' @col2.b the colour of the husbands' circle border
+#' 
+#' the circle drawing function from `plotrix` takes care of the aspect ratio 
+#' So it changes the y limits depending on the output device
 
+
+FunPlotBase <- function(i, col1, col1.b, col2, col2.b) {
+  df <- readRDS(catalog$final.rds[i])
+  pt <- prop.table(xtabs(df$weight ~ df$var + df$region), 2)
+  fit <-eulerr::euler(c(
+        "wife-urban" = pt[1, 1],
+        "husband-urban" = pt[2, 1],
+        "wife-urban&husband-urban" = pt[3, 1],
+        "wife-rural" = pt[1, 2],
+        "husband-rural" = pt[2, 2],
+        "wife-rural&husband-rural" = pt[3, 2]))
+  f <- fit$coefficients
+  dist.u <- ((f[1, 1] - f[2, 1]) ^ 2 + (f[1, 2] - f[2, 2]) ^ 2) ^ 0.5
+  dist.r <- ((f[3, 1] - f[4, 1]) ^ 2 + (f[3, 2] - f[4, 2]) ^ 2) ^ 0.5
+  
+  # plot empty plot 
+  plot(1, 
+    xlim = c(-1.5, 1.5),
+    ylim = c(-1, 1),
+    type = "n",
+    bty = "n",
+    ylab = "",
+    xlab = "",
+    axes = FALSE )
+  
+  # title
+  mtext(paste(catalog$country[i], catalog$year[i], sep = " - "))
+  # rural wives
+  plotrix::draw.circle(-0.7, 0, f[3, 3], 
+              col = col1,
+              border = col1.b)
+  # rural husbands
+  plotrix::draw.circle(-0.7, dist.r, f[4, 3],
+              col = col2, 
+              border = col2.b)
+  # urban wives
+  plotrix::draw.circle(0.7, 0, f[1, 3], 
+              col = col1, 
+              border = col1.b)
+  # urban husbands
+  plotrix::draw.circle(0.7, dist.u, f[2, 3],
+              col = col2,
+              border = col2.b)
+}
+
+
+FunPlotBar <- function(i, pal) {
+  df <- readRDS(catalog$final.rds[i])
+  pt <- prop.table(xtabs(df$weight ~ df$var + df$region), 2)
+  barplot(pt[c(1,3,2,4), ], col = rgb(pal$RGB), border = rgb(pal$RGB))
+  # title
+  mtext(paste(catalog$country[i], catalog$year[i], sep = " - "), 
+        line = 2)
+}
 
 FunPlotSimple <- function(i){
   df <- readRDS(catalog$final.rds[i])
   pt <- prop.table(xtabs(df$weight ~ df$var + df$region), 2)
-  fit1 <- euler(c("wife-urban" = pt[1,1], "husband-urban" = pt[2,1], "wife-urban&husband-urban" = pt[3,1],
+  fit1 <- eulerr::euler(c("wife-urban" = pt[1,1], "husband-urban" = pt[2,1], "wife-urban&husband-urban" = pt[3,1],
                   "wife-rural" = pt[1,2], "husband-rural" = pt[2,2], "wife-rural&husband-rural" = pt[3,2]))
   plot(fit1)
 }
