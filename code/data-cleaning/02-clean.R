51f4bf16e265e992b66cbdacb9cc7bffadada4e9
majazaloznik <maja.zaloznik@gmail.com>
2018-01-14 18:15:29 +0000

    merge with UN data, layout summary

--- code/data-cleaning/02-clean.R
+++ code/data-cleaning/02-clean.R
@@ -20,10 +20,14 @@
 #' 
 ## 0. Preliminaries  ##########################################################
 ###############################################################################
+library(dplyr)
+library(tidyr)
 source("code/functions/FunDataExtractor.R")
 catalog <- readRDS("data/interim/catalog.rds")
 # add new empty variable to keep track of which files have been extracted
 catalog$v632 <- NA
+# import UN regional codes list
+UNcodes <- read.csv("data/raw/UNcodes.csv", stringsAsFactors = FALSE)
 
 # 1.Extract only required variables############################################
 ###############################################################################
@@ -43,10 +47,36 @@ catalog <- dplyr::filter(catalog, v632 == TRUE)
 catalog$full.rds <- paste0("data/interim/",catalog$filecode, ".rds")
 catalog$final.rds <- paste0("data/processed/",catalog$filecode, ".rds")
 
-#  remove the second dominican file that is only sugar plantatin workers
+#  remove the second dominican file that is only sugar plantation workers
 catalog <- dplyr::filter(catalog, output_filename != "data/raw/DRIR5ADT.zip")
+
+# rename countries to UN standard
+catalog$country <- as.character(catalog$countr)
+# rrename to UN standard country names
+catalog[catalog$country == "Congo Democratic Republic",2] <- "Democratic Republic of the Congo"
+catalog[catalog$country == "Bolivia",2] <- "Bolivia (Plurinational State of)"
+catalog[catalog$country == "Cote d'Ivoire",2] <- "Côte d'Ivoire"
+catalog[catalog$country == "Kyrgyz Republic",2] <- "Kyrgyzstan"
+catalog[catalog$country == "Moldova",2] <- "Republic of Moldova"
+catalog[catalog$country == "Tanzania",2] <- "United Republic of Tanzania"
+# join with regional table
+catalog <- left_join(catalog, UNcodes, by = c("country" = "Country.or.Area"))
 
 #save catalog
 write.csv(catalog, "data/processed/catalog.final.csv")
 
+# layout helper table
+# layout help:
 
+layout.summary <- data_frame(country = catalog$country,
+                             year = catalog$year, 
+                             region = catalog$Region.Name,
+                             subregion = ifelse(catalog$Sub.region.Name == "Sub-Saharan Africa", catalog$Intermediate.Region.Name,
+                                                catalog$Sub.region.Name))
+layout.summary %>%  group_by(country) %>%
+  mutate(no = row_number()) %>%
+  spread(no, year, fill = "") %>% 
+  arrange(region, subregion) ->layout.summary
+
+#save catalog
+write.csv(layout.summary, "data/processed/layout.summary.csv")
